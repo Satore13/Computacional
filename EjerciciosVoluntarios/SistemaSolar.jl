b1ff1e00026f1e354c82d8f7471db2d26fb42498
Seth <53190848+Xx-Seth-xX@users.noreply.github.com>
2022-03-10 03:59:38 +0100

    Añadidos los datos empíricos que restaban y terminado la simulación base

--- EjerciciosVoluntarios/SistemaSolar.jl
+++ EjerciciosVoluntarios/SistemaSolar.jl
@@ -1,7 +1,7 @@
 include("Verlet.jl")
 Particle, Frame = Verlet.Particle, Verlet.Frame
 import Base.string
-using CSV
+using CSV, Plots, DataFrames
 using DataStructures
 
 #=
@@ -12,9 +12,11 @@ Las unidades de masa son masas solares, las de distancia son la distancia tierra
 toOwnUnitsMass(m::Float64) = m/1.989e30 
 toOwnUnitsTime(t::Float64) = t * √(1.989e30*6.67384e-11/((1.496e11)^3))
 toOwnUnitsMeters(d::Float64) = d/1.496e11 
+toOwnUnitsSpeed(v::Float64) = v/(1.496e11 *√(1.989e30*6.67384e-11/((1.496e11)^3)))#toOwnUnitsMeters(fromOwnUnitsTime(v))
 fromOwnUnitsMass(m::Float64) = m * 1.989e30 
 fromOwnUnitsTime(t::Float64) = t / √(1.989e30*6.67384e-11/((1.496e11)^3))
 fromOwnUnitsMeters(d::Float64) = d * 1.496e11 
+fromOwnUnitsSpeed(v::Float64) = fromOwnUnitsMeters(toOwnUnitsTime(v))
 
 
 
@@ -25,41 +27,46 @@ function norm(x::Vector{Float64})::Float64
     end
     return √(normsq)
 end
-function gravitationalAcc(p::Particle, f::Frame)
+function gravitationalAcc(p::Particle, f::Frame)::Vector{Float64}
     m_sol = 1
     
     #Interacciones entre los planetas
-    total_f = 0.0
-    total_f = (sum(f.particles) do other_p
+    total_f = zeros(length(p.x))
+    #= total_f = (sum(f.particles) do other_p
         if(other_p ≢ p)
             return (1 / norm(p.x - other_p.x)^3) * (-p.x)
         end
         return zeros(length(p.x))
-    end)
+    end) =#
     #Interacción planeta-sol
     total_f += (1 / norm(p.x)^3) .* (-p.x)
     
     return total_f
 end
-
+    
 # Parametros de la simulacion
 FPS = 24
-
-length_of_sim = 5
-step = 0.001
+playback_speed = 0.5
+length_of_sim = 10
+step = 0.0001
 
 global PlanetsIndex = DataStructures.OrderedDict{Symbol, Int64}(:Mercurio => 1, :Venus=> 2, :Tierra => 3, :Marte => 4, :Jupiter => 5, :Saturno => 6, :Urano => 7, :Neptuno => 8)
 
-# Masas de los planetas en unidades de 10^24 metros que pasamos por nuestra función de conversión
-global PlanetsMasses = toOwnUnitsMass.([0.330, 4.87, 5.97, 0.073, 0.642, 1899, 568, 86.8, 102, 0.0125].*1e24)
-    
-particleFromPlanet(i::Int64) = Particle([1.0*i, 0.0],[0.0, i*1.0], PlanetsMasses[i])
-global list_of_planets = [particleFromPlanet(index) for index in values(PlanetsIndex)]::Vector{Particle}
+begin
+    # Masas de los planetas en unidades de 10^24 metros que pasamos por nuestra función de conversión
+    global PlanetsMasses = toOwnUnitsMass.([0.330, 4.87, 5.97, 0.073, 0.642, 1899, 568, 86.8, 102, 0.0125].*1e24)
 
+    #Distancias planeta-sol
+    global PlanetsDistance = [0.387, 0.723, 1, 1.52, 5.20, 9.57, 19.17, 30.18, 39.48]
 
+    #Velocidad orbital (km/s)
+    global PlanetsSpeed = toOwnUnitsSpeed.([47.9, 35.0, 29.8, 24.1, 13.1, 9.7, 6.8, 5.4, 4.7].*1e3)
 
+    particleFromPlanet(i::Int64) = Particle([PlanetsDistance[i] ; 0.0],[0.0; PlanetsSpeed[i]], PlanetsMasses[i])
+    global list_of_planets = [particleFromPlanet(index) for index in values(PlanetsIndex)]::Vector{Particle}
+end
 
-function run()
+function runSimulation()
     initial_frame = Frame(0.0, list_of_planets)
     simulation_data = Verlet.dataFrameRowFromFrame(initial_frame)
     
@@ -79,4 +86,21 @@ function run()
     CSV.write("datos_sistema_solar.out", simulation_data)
 end
 
+function buildAnimation()
+    df = DataFrame(CSV.File("datos_sistema_solar.out"))
 
+    animation = Animation()
+    println("FPS = " * string(FPS))
+    for row in eachrow(df)
+        frame_plot = plot()
+        #Dibujar todos los planetas
+        for (plabel, index) in PlanetsIndex
+            scatter!(frame_plot, [row["p"*string(index)*"x1"]], [row["p"*string(index)*"x2"]], label=string(plabel), xlims=(-5,5), ylim = (-5,5))
+        end
+        xlabel!("t = "*string(round(row.time, digits = 2)))
+        #Dibujar el sol
+        scatter!(frame_plot, [0.0], [0.0], label="Sol")
+        frame(animation, frame_plot)
+    end
+    return gif(animation, fps = FPS)
+end
