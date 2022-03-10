b0e754984f76421d2b95c9a56689c6813ab2ab32
Seth <53190848+Xx-Seth-xX@users.noreply.github.com>
2022-03-10 18:43:32 +0100

    Modificado estructura de archivos

--- Tarea1/SistemaSolar.jl
+++ Tarea1/SistemaSolar.jl
@@ -32,12 +32,12 @@ function gravitationalAcc(p::Particle, f::Frame)::Vector{Float64}
     
     #Interacciones entre los planetas
     total_f = zeros(length(p.x))
-    #= total_f = (sum(f.particles) do other_p
+    total_f = (sum(f.particles) do other_p
         if(other_p ≢ p)
-            return (1 / norm(p.x - other_p.x)^3) * (-p.x)
+            return (1 / norm(p.x - other_p.x)^3) * (-p.x) * other_p.mass
         end
         return zeros(length(p.x))
-    end) =#
+    end) 
     #Interacción planeta-sol
     total_f += (1 / norm(p.x)^3) .* (-p.x)
     
@@ -45,11 +45,11 @@ function gravitationalAcc(p::Particle, f::Frame)::Vector{Float64}
 end
     
 # Parametros de la simulacion
-FPS = 24
-playback_speed = 0.5
-length_of_sim = 10
-step = 0.0001
-
+begin
+    FPS = 10
+    length_of_sim = 100
+    step = 0.0001
+end
 global PlanetsIndex = DataStructures.OrderedDict{Symbol, Int64}(:Mercurio => 1, :Venus=> 2, :Tierra => 3, :Marte => 4, :Jupiter => 5, :Saturno => 6, :Urano => 7, :Neptuno => 8)
 
 begin
@@ -86,6 +86,11 @@ function runSimulation()
     CSV.write("datos_sistema_solar.out", simulation_data)
 end
 
+begin
+    #Parámetros de la animación
+    width = (-50, 50)
+    height = (-50, 50)
+end
 function buildAnimation()
     df = DataFrame(CSV.File("datos_sistema_solar.out"))
 
@@ -95,12 +100,12 @@ function buildAnimation()
         frame_plot = plot()
         #Dibujar todos los planetas
         for (plabel, index) in PlanetsIndex
-            scatter!(frame_plot, [row["p"*string(index)*"x1"]], [row["p"*string(index)*"x2"]], label=string(plabel), xlims=(-5,5), ylim = (-5,5))
+            scatter!(frame_plot, [row["p"*string(index)*"x1"]], [row["p"*string(index)*"x2"]], label=string(plabel), xlims=width, ylim = height)
         end
-        xlabel!("t = "*string(round(row.time, digits = 2)))
+        xlabel!("t = "*string(round(row.time*58.1, digits = 2))*" días")
         #Dibujar el sol
         scatter!(frame_plot, [0.0], [0.0], label="Sol")
         frame(animation, frame_plot)
     end
-    return gif(animation, fps = FPS)
+    gif(animation, "Animacion.gif",fps = FPS)
 end
