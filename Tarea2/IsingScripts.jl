8c566db863babe3bbdd7f261bd6255048ee49762
Seth <53190848+Xx-Seth-xX@users.noreply.github.com>
2022-04-26 16:52:58 +0200

    Arreglado función sobre la que ajustar M(T), ahora coincide con la de la solución analítica de Onsager

--- Tarea2/IsingScripts.jl
+++ Tarea2/IsingScripts.jl
@@ -1,7 +1,8 @@
 include("ModeloIsing.jl")
 using GLMakie
 using Serialization
-using SciPy
+using LsqFit
+using Measurements
 function plotear_EyMvspMC(datos::Vector{Red}, guardadas_cada::Integer = 1, filename::Union{Nothing, String} = nothing)
     N = size(datos[1])
     T = round(digits = 5, datos[1].Temperatura)
@@ -75,28 +76,11 @@ end
 function plotear_MvsT!(ax::Axis, label, data::Vector{Tuple{Float64, Float64, Float64}})
     x = getindex.(data, 1)
     yM = getindex.(data, 3)
-    scatter!(ax, x,  yM, label = label)
+    scatter!(ax, x,  yM, label = label, color = :black)
 end
 
 function procesar_EyMvsT(n)
     T = collect(range(0.1, 5, step = 0.1))
-    output = Tuple{Float64, Float64, Float64}[]
-    gc = 10
-    for t in T
-        t = round(digits = 5, t)
-        
-        filename = "Tarea2/output/n$n"*"T$t"*"gc$gc"*".out"
-        println("Cargando archivo: $filename")
-        data = deserialize(filename)[(end - 200):end]
-        push!( output, (t, average_E(data), abs(average_M(data))) )
-    end
-    return output
-end
-
-
-function procesar_EyMvsTzoom(n)
-    T = collect(range(1.9, 3.0, length = 40))
-    T = round.(T, digits = 5)
     output = Tuple{Float64, Float64, Float64}[]
     gc = 10
     for t in T
@@ -125,44 +109,18 @@ function plotear_EvsTvsn()
     return fig
 end
 function plotear_MvsTvsn()
-    N = [16, 32, 64, 128]
+    N = [128]
     fig = Figure()
     ax = Axis(fig[1, 1])
     ylims!(ax, (-0.2, 1.2))
-    xlims!(ax, (0, 5))
+    xlims!(ax, (1, 4))
     for n in N
-        plotear_MvsT!(ax, "N = $n" , deserialize("Tarea2/output/$(n)E_MvsT.out"))
+        plotear_MvsT!(ax, "N = $n" , deserialize("Tarea2/output/n$(n)EyMvsT.out"))
     end
     axislegend(ax, position = :rt)
     return fig
 end
 
-function plotear_EvsTvsnzoom()
-    N = [16, 32, 64, 128]
-    fig = Figure()
-    ax = Axis(fig[1, 1])
-
-    ylims!(ax, (-2.2, -0.2))
-    xlims!(ax, (1.8, 3.1))
-
-    for n in N
-        plotear_EvsT!(ax, "N = $n" , deserialize("Tarea2/output/$(n)E_MvsTzoom.out"))
-    end
-    axislegend(ax, position = :rb)
-    return fig
-end
-function plotear_MvsTvsnzoom()
-    N = [16, 32, 64, 128]
-    fig = Figure()
-    ax = Axis(fig[1, 1])
-    ylims!(ax, (-0.2, 1.2))
-    xlims!(ax, (1.8, 3.1))
-    for n in N
-        plotear_MvsT!(ax, "N = $n" , deserialize("Tarea2/output/$(n)E_MvsTzoom.out"))
-    end
-    axislegend(ax, position = :rt)
-    return fig
-end
 
 function plotear_correlacion(red::Red)
     max_val = ceil(Int64, size(red)/2)
@@ -174,35 +132,63 @@ function plotear_correlacion(red::Red)
 
 end
 
-function ajuste_MvsT(filename::String)
+function ajuste_MvsT!(ax, filename::String)
     data = deserialize(filename)
-    x = getindex.(data[1:20], 1)
-    y = getindex.(data[1:20], 3)
-    @show x
-    @show y
-    function sigmoid(β, x)
-        1/(1 .+ exp.((x .- β[1]) .* 50))
+    x = Float64[]
+    y = Float64[]
+    for d in data
+        if d[1] < 2.26 && d[3] > 0.8
+            push!(x, d[1])
+            push!(y, d[3])
+        end
     end
-
-    odr_data = SciPy.odr.RealData(x, y)
-    odr_model = SciPy.odr.Model(sigmoid)
-    odr_job = SciPy.odr.ODR(odr_data, odr_model, [2.65])
-
-    odr_output = odr_job.run()
+    @.  model(x, p) =  (p[1] - p[2]/(sinh(2/x)^4))^p[3]
     
-    return (odr_output.beta, odr_output.sd_beta)
+    p0 = [1.0, 0.01, 1.0]
+    fit = curve_fit(model, x, y, p0)
+    #fit = LsqFit.LsqFitResult(fit.)
+    p = coef(fit) .± stderror(fit)
+    @show p
+    @show Tc = 2/(asinh(  (p[2]/p[1]) ^ (1/4)) )   
+
+    plotear_MvsT!(ax, "", data)
+
+    x = collect(range(x[1], Measurements.value(Tc - 0.001), length = 100))
+    y = model(x, coef(fit))
+    lines!(ax, x, y, color = :red)
 end
 
-#=
-function ajuste(x, y, xe, ye; model = line, seed)
-    odr_data = SciPy.odr.RealData(x, y, xe, ye)
-    odr_model = SciPy.odr.Model(model)
-    odr_job = SciPy.odr.ODR(odr_data, odr_model, seed)
-    odr_output = odr_job.run()
-    ajuste = DataFrame()
-    ajuste.β = odr_output.beta
-    ajuste.βe = odr_output.sd_beta
-    ajuste.β_ = ajuste.β .± ajuste.βe
-    return ajuste
+function ajuste_MvsT(filename::String)
+    ajuste_MvsT!(Axis(Figure()[1, 1]), filename)
+    current_figure()
 end
-=#
+
+function plotear_ajustes()
+    filenames = "Tarea2/output/2_5to3_5_segundo/n" .* ["16", "32", "64"#=, "128"=#] .* "EyMvsT.out"
+    titles = ["16x16", "32x32", "64x64"]#, "128x128"]
+    fig = Figure()
+
+    axes = [Axis(fig[i, j]) for i in 1:2, j in 1:2]
+
+    @show filenames
+    @show axes
+    
+    for (ax, file, title) in zip(axes,  filenames, titles)
+        ajuste_MvsT!(ax, file)
+        ax.title = title
+    end
+    return fig
+end
+
+function purgar_datos(filename::String)
+    data = deserialize(filename)
+    @assert typeof(data) == Vector{Tuple{Float64, Float64, Float64}}
+    new_data = empty(data)
+    for d in data
+        if (d[1] < 2.26) && (d[3] < 0.6)
+            continue
+        end
+        push!(new_data, d)
+    end
+    serialize(filename, new_data)
+end
