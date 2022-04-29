f6f03286e639778c8f421b458ab6461e5d599cca
Seth <53190848+Xx-Seth-xX@users.noreply.github.com>
2022-04-29 20:15:24 +0200

    Hecho el ejercicio obligatorio del Tema 3 - Ecuación de Schrödinger -

---
+++ Tarea3/Schrodinger.jl
@@ -0,0 +1,192 @@
+using OffsetArrays
+#= 
+    Forma rápida de crear un vector con origen en el 0 a partir del paquete OffsetArrays
+
+=#
+Vector0{T} = OffsetVector{T, Vector{T}}
+function Vector0(T::Type)
+    OffsetVector(T[], OffsetArrays.Origin(0))
+end
+function Vector0(T::Type, size)
+    OffsetVector(Vector{T}(undef, size), OffsetArrays.Origin(0))
+end
+function Vector0(v::Vector)
+    OffsetVector(v, OffsetArrays.Origin(0))
+end
+struct WaveFunction
+    val::Vector0{ComplexF64}
+    h::Float64
+    WaveFunction(val::Vector0{ComplexF64}) = new(val, 1.0)
+end
+function (φ::WaveFunction)()
+    return φ.val
+end
+function (φ::WaveFunction)(j::Int64)
+    return φ.val[j]
+end
+
+
+function Base.size(φ::WaveFunction)
+    return size(φ(), 1)
+end
+
+function norm(φ::WaveFunction)::Float64
+    sqrt(sum(abs2.(φ()))) 
+end
+function normalize(φ::WaveFunction)::WaveFunction
+    normφ = norm(φ)
+    WaveFunction( [ φ(j) / normφ for j in eachindex(φ())] )
+end
+function probability_density(φ::WaveFunction, j::Int64)::Float64
+    abs2.(φ(j))
+end
+
+function generar_onda_gaussiana(j0, k0, σ, N)::WaveFunction
+    normalize(WaveFunction(Vector0([cis( (j * k0)) * exp(-8*(4*j - N)^2 / N^2) for j in 0:N])))
+end
+
+struct QOperator
+    f::Function
+end
+function Base.:+(φ, ψ)
+    if size(φ) ≠ size(ψ)
+        @error("Las funciones de onda tienen que estar definidas sobre el mismo intervalo en el espacio de posiciones")
+    end
+    WaveFunction([φ(j) + ψ(j) for j in eachindex(φ())])
+end
+function (f::QOperator)(φ::WaveFunction)::WaveFunction
+    return f.f(φ)
+end
+
+function mean_value(φ::WaveFunction, A::QOperator)
+    ψ = A(φ)
+    real(sum( [ adjoint(phi_n) * psi_n for (phi_n, psi_n) in zip(φ(), ψ())] ))
+end
+
+position_op = QOperator(
+    (φ) -> WaveFunction([ i*φ(i) for i in eachindex(φ())])
+)
+momentum_op = QOperator(
+    (φ) -> begin
+        N = size(φ) - 1
+        ψ_val = Vector0(ComplexF64, N + 1)
+        ψ_val[N] = 0.0
+
+        for j in 0:(N-1)
+            ψ_val[j] = (φ(j+1) - φ(j)) * (-im)
+        end
+        WaveFunction(ψ_val)
+    end
+)
+function build_potential_op(V::Vector0{ComplexF64})::QOperator
+    QOperator((φ) -> WaveFunction([ V[i]*φ(i) for i in eachindex(φ())]))
+end
+function build_energy_op(V::QOperator)::QOperator
+    return QOperator(
+        (φ) -> begin
+            momentum_op(momentum_op(φ)) + V(φ)
+        end
+    )
+end
+
+
+struct Simulation
+    N::Int64
+    n_ciclos::Int64
+    s::Float64
+    
+    φn::Vector0{WaveFunction}
+    k0::ComplexF64
+    α::Vector0{ComplexF64}
+    V::Vector0{ComplexF64}
+    A::Vector0{ComplexF64}
+end
+
+function calcular_α(N::Int64, A::Vector0{ComplexF64})
+    α = Vector0(ComplexF64, N)
+    α[N-1] = 0.0
+    for j in (N-1):-1:1
+        γj = (A[j] + α[j])^-1
+        α[j-1] = -γj
+    end
+    return α
+end
+
+function calcular_βn(N::Int64, A::Vector0{ComplexF64}, bn::Vector0{ComplexF64}, α::Vector0{ComplexF64})
+    βn = Vector0(ComplexF64, N)
+    βn[N-1] = 0.0
+    for j in (N-1):-1:1
+        γj = (A[j] + α[j])^-1
+        βn[j - 1] = γj * (bn[j] - βn[j])
+    end
+    return βn
+end
+
+function calcular_bn(φ::WaveFunction, s::Float64)
+    return [4 * im * φj / s for φj in φ()]
+end
+
+function calcular_A(V::Vector0{ComplexF64}, s::Float64)
+    return [-2 + 2 * im / s - Vj for Vj in V]
+end
+
+function calcular_χn(N::Int64, A::Vector0{ComplexF64}, bn::Vector0{ComplexF64}, α::Vector0{ComplexF64}, βn::Vector0{ComplexF64})::Vector0{ComplexF64}
+    #Lo primero es calcular las A
+    χ = Vector0(ComplexF64, N + 1)
+    χ[0] = χ[N] = 0.0
+    for j in 1:(N-1)
+        χ[j] = - 1/ (A[j] + α[j]) * χ[j - 1] + (bn[j] - βn[j])/(A[j] + α[j])
+    end
+    return χ
+end
+
+function generar_simulacion(N, n_ciclos, x0 = N/4, σ = N/16, choice_of_potential::Symbol = :none, Λ::Vector = [])::Simulation
+    #Valores independientes del tiempo
+    k0 = 2π * n_ciclos / N
+    if choice_of_potential == :none
+        V = Vector0([Complex(0.0) for _ in 0:N])
+    elseif choice_of_potential == :square
+        V = Vector0([begin
+            if 2 * N / 5 ≤ j ≤ 3*N/5
+                Complex(Λ[1]*k0^2)
+            else 
+                Complex(0.0)
+            end
+        end for j in 0:N])
+        @show V
+    end
+    
+    #Calcular configuraciones iniciales
+    φ0 = generar_onda_gaussiana(x0, k0, σ, N)
+    s = 1/(4*abs2(k0))
+    @show s
+    A = calcular_A(V, s)
+    α = calcular_α(N, A)
+
+    return Simulation(N, n_ciclos, s, Vector0(WaveFunction[φ0]), k0, α, V, A)
+end
+
+function paso_simulacion!(simulacion::Simulation)
+    current_wf = simulacion.φn[end]
+    s = simulacion.s
+    N = simulacion.N
+    A = simulacion.A
+    α = simulacion.α
+    current_b = calcular_bn(current_wf, s)
+    current_β = calcular_βn(N, A, current_b, α)
+    current_χ = calcular_χn(N, A, current_b, α, current_β)
+    new_wf = [current_χ[j] - current_wf(j) for j in eachindex(current_wf())]
+
+    push!(simulacion.φn, WaveFunction(new_wf))
+end
+
+function bucle_simulacion!(simulation::Simulation, pasos::Integer)
+    for i in 1:pasos
+        paso_simulacion!(simulation)
+    end
+end
+function bucle_simulacion(N, n_ciclos, pasos; x0 = N/4, σ = N/16, choice_of_potential::Symbol = :none, Λ::Vector = [])
+    sim = generar_simulacion(N, n_ciclos, x0, σ, choice_of_potential, Λ)
+    bucle_simulacion!(sim, pasos)
+    return sim
+end
---
+++ Tarea3/SchrodingerScripts.jl
@@ -0,0 +1,75 @@
+include("Schrodinger.jl")
+using GLMakie
+using Serialization
+
+function plotear_probabilidad!(ax, φ::WaveFunction, color = :blue)
+    N = size(φ) - 1
+    x = collect(0:N)
+    y = [probability_density(φ, xi) for xi in x]
+    @show y
+    lines!(ax, x, y, color = color)
+end
+
+function plotear_probabilidad(φ::WaveFunction)
+    ax = Axis(Figure()[1, 1])
+    plotear_probabilidad!(ax, φ)
+    current_figure()
+end
+
+function plotear_anim(sim::Simulation, filename::String="Tarea3/videos/sim.mp4")
+    N = sim.N
+    x = collect(0:N)
+    index = Observable{Int64}(1)
+    φn = @lift(sim.φn[$index])    
+    energy_op = build_energy_op(build_potential_op(sim.V))
+
+    fig = Figure()
+
+    ax_wf = Axis(fig[1, 1:3], xlabel = "x")
+    ax_potential = Axis(fig[1, 1:3], yaxisposition = :right)
+    hidespines!(ax_potential)
+    hidexdecorations!(ax_potential)
+    ρ = @lift(Vector0([probability_density($φn, i) for i in 0:N]))
+    density_dots = @lift([Point2f(i, $ρ[i]) for i in 0:N])
+    plt_density = lines!(ax_wf, density_dots)
+    mean_x = @lift(mean_value($φn, position_op))
+    plt_mean_x = vlines!(ax_wf, mean_x, color = :black)
+    potential_dots = [Point2f(j, sim.V[j]) for j in 0:N]
+    plt_potential = lines!(ax_potential, potential_dots, color = :red)
+    plt_energy = hlines!(ax_potential, mean_value(sim.φn[0], energy_op), color = :orange)
+    xlims!(ax_wf, (0, N))
+    xlims!(ax_potential, (0, N))
+    Legend(fig[1, 4], [plt_density, plt_mean_x, plt_potential, plt_energy], ["Densidad de probabilidad", "<x>", "V(x)", "<H>"])
+
+    ax_parts = Axis(fig[2, 1:3], xlabel = "x")
+    imag_dots = @lift([Point2f(i, imag($φn.val[i])) for i in 0:N])
+    real_dots = @lift([Point2f(i, real($φn.val[i])) for i in 0:N])
+    lines!(ax_parts, imag_dots, label = "Parte imaginaria")
+    lines!(ax_parts, real_dots, label = "Parte real")
+    xlims!(ax_parts, (0, N))
+    Legend(fig[2, 4], ax_parts)
+
+    ax_norm = Axis(fig[4, 1:3], xlabel = "t")
+    norm_dots = Observable(Point2f[(-1,0)])
+    lines!(ax_norm, norm_dots, label = "|φ|²")
+    ylims!(0, 1.1)
+    xlims!(0, eachindex(sim.φn)[end])
+    Legend(fig[4, 4], ax_norm)
+
+    ax_momentum = Axis(fig[3, 1:3], xlabel = "t")
+    momentum_dots = Observable(Point2f[(0, sim.k0)])
+    energy_dots = Observable(Point2f[(-1, 0)])
+    lines!(ax_momentum, momentum_dots, label = "<p>")
+    lines!(ax_momentum, energy_dots, label = "<H>")
+    xlims!(ax_momentum, (0, eachindex(sim.φn)[end]))
+    ylims!(ax_momentum, (-sim.k0, sim.k0))
+    Legend(fig[3, 4], ax_momentum)
+
+
+    record(fig, filename, eachindex(sim.φn),framerate = 30) do i
+        index[] = i
+        norm_dots[] = push!(norm_dots[], Point2f(i, norm(sim.φn[i])))
+        momentum_dots[] = push!(momentum_dots[], Point2f(i, mean_value(sim.φn[i], momentum_op)))
+        energy_dots[] = push!(energy_dots[], Point2f(i, mean_value(sim.φn[i], energy_op)))
+    end
+end
